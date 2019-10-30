d961a358d2b4e8b81e46e0f08f3fa5b006a4744d
Douglas Kane <douglas.kane@034gc.onmicrosoft.com>
2019-10-29 21:34:17 -0400

    Added Add Rating

--- RateAppWebProject/Controllers/HomeController.cs
+++ RateAppWebProject/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RateAppWebProject.Models;
 
 namespace RateAppWebProject.Controllers
 {
+    // [Authorize]
     public class HomeController : Controller
     {
         public async Task<IActionResult> Index()
@@ -41,5 +42,28 @@ namespace RateAppWebProject.Controllers
             }
             return View(rating);
         }
+
+        public ViewResult AddRating() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddRating(Ratings rating)
+        {
+            Ratings receivedRating = new Ratings();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(rating), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("http://tcapprateapi.azurewebsites.net/api/Commands/", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedRating = JsonConvert.DeserializeObject<Ratings>(apiResponse);
+                }
+            }
+            return View(receivedRating);
+        }
+
     }
+
+
+
 }
